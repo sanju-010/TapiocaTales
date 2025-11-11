e096b78a40381d2bd90c310e6fa89a6f234bc94c
sanju <sanjuworkmail459@gmail.com>
2025-11-11 09:38:01 +0530

    Initital Upload.

---
+++ TapiocaTales/Controllers/AdminAuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TapiocaTales.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminAuthController : ControllerBase
+    {
+    }
+}
